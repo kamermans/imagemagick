edc510059e9a14b6907498fdbd09134d6be26ab3
Steve Kamerman <stevekamerman@gmail.com>
2018-04-29 21:08:26 -0400

    Actual runs can vary depending on CPU count/performance

--- imagemagick_test.go
+++ imagemagick_test.go
@@ -379,8 +379,8 @@ func TestGetImageDetailsParallel(t *testing.T) {
 
 	expectedRuns := numTestFiles / parser.BatchSize
 	actualRuns := mockExec.RunCount()
-	if expectedRuns != actualRuns {
-		t.Fatalf("GetImageDetailsParallel() failed: wrong run count: expected %v got %v", expectedRuns, actualRuns)
+	if expectedRuns < actualRuns {
+		t.Fatalf("GetImageDetailsParallel() failed: wrong run count: expected >= %v got %v", expectedRuns, actualRuns)
 	}
 
 	expectedCmd := "convert"
